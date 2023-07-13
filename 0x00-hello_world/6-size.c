5ba37a62a9275cb8a5135d8b89d24a5b880905a1
R-HYTE <polycarpwathuta39@gmail.com>
2023-07-13 21:02:23 +0300

    Prints size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints various siaes of data types
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a : %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of b : %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of c : %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of d : %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of e : %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
