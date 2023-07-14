80cf283d7f01fc4618310cf869abf6bfd9b3e1d3
R-HYTE <polycarpwathuta39@gmail.com>
2023-07-15 01:39:59 +0300

    Prints lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char myChar;
+
+	myChar = 'z';
+	while
+		(myChar >= 'a')
+		{
+			putchar(myChar);
+			myChar--;
+		}
+	putchar('\n');
+	return (0);
+}
