e0546fd2861f800dd40de026615a84c608d0c6f1
R-HYTE <polycarpwathuta39@gmail.com>
2023-07-18 23:50:37 +0300

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+ /**
+  * jack_bauer - Print minute of the day of Jack Bauer
+  * @void: No parameters being passed
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < 24)
+	{
+		b = 0;
+		while (b < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(58);
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+}
