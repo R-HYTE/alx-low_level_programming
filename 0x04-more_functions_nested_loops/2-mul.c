ae73b9f22e2ca2d1d93ca36fbe5e7116b1845485
R-HYTE <polycarpwathuta39@gmail.com>
2023-07-21 01:34:03 +0300

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - checks for a digit (0 through)
+ * @a: Variable
+ * @b: Variable
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
