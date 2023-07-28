d22bc1f09f6ee1c7151e3fc98a2e13271200f82a
R-HYTE <polycarpwathuta39@gmail.com>
2023-07-28 17:53:18 +0300

    inds and prints the first 98 Fibonacci numbers
    starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 /**
- * main - Prints the fibonacci series
+ * main - Prints the fibonacci series of the first 98
  * Return: 0 Always success
  */
 int main(void)
 {
-	int i, n = 98;
-	unsigned long term1 = 1, term2 = 2;
-	unsigned long nextTerm = term1 + term2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%lu, %lu, ", term1, term2);
-	for (i = 3; i <= n; ++i)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+	for (i = 1; i < 91; ++i)
 	{
-		printf("%lu, ", nextTerm);
-		term1 = term2;
-		term2 = nextTerm;
-		nextTerm = term1 + term2;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
