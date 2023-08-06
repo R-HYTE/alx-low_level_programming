a3e6da7285c3e1c2a69b3a8898a543bbfd8296a5
R-HYTE <polycarpwathuta39@gmail.com>
2023-08-06 18:10:36 +0300

    static library libmy.a containing functions

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: value being computed
+ * Return: Absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
