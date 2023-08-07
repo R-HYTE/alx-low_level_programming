6d161bbf0b576bd1493ff9904905209400aeb439
R-HYTE <polycarpwathuta39@gmail.com>
2023-08-07 13:25:41 +0300

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints it's name
+ * @argc: command line argument count
+ * @argv: command line argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
