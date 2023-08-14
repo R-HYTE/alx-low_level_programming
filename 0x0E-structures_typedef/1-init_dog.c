68877414eaca5e3a3e855de68c835b6c0d9931de
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-14 23:27:14 +0300

    Added a library

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - Function initializes a var of type struct
@@ -9,6 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
 	d->name = name;
 	d->age = age;
 	(*d).owner = owner;
