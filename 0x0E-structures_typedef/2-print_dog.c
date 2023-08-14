5239bf2c0dd9a61c56c9f661faaa249547b429d8
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-14 23:57:12 +0300

    We are tod if d is NULL print nothing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_dog - prints a struct dog
@@ -10,7 +9,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
