1969f73c6fc492a4fef695b790fcfd64b1adf1d9
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-14 22:58:45 +0300

    Added comments in the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef dog_h
 #define dog_h
+/**
+ *struct dog - this contains structure of a dog
+ *@name: member of the strucrure
+ *@age: member of the structure
+ *@owner: member of the structure
+ */
 
 struct dog
 {
