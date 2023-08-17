31463dde247ae37bfdf34c9882d2a215ef2f1114
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-17 14:48:17 +0300

    Bug fixed

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - A pointer to a function that returns an address
+ * @s: argument of this function
+ * Return: address to a function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; ops[i].op != NULL && *(ops[i].op) != *s;)
+		i++;
+	return (ops[i].f);
+}
+
