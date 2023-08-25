4d1cf9728ccf7f2eb8e1c351433f9e70ec2e0df2
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-25 15:27:48 +0300

    Ended up using print to fix bug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,58 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0, j;
+	size_t i = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
-		const char *str = h->str;
-		unsigned int len = h->len;
-
-		if (str == NULL)
-		{
-			_putchar('[');
-			str = "(nil)";
-			len = 0;
-		}
-
-		if (len > 0)
-		{
-			unsigned int temp = len;
-			unsigned int divisor = 1;
-			while (temp / 10 > 0)
-			{
-				divisor *= 10;
-				temp /= 10;
-			}
-
-			_putchar('[');
-			while (divisor > 0)
-			{
-				_putchar((len / divisor) + '0');
-				len %= divisor;
-				divisor /= 10;
-			}
-		}
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-		{
-			_putchar('0');
-		}
+			printf("[%u] %s\n", h->len, h->str);
 
-		_putchar(']');
-		_putchar(' ');
-
-		for (j = 0; j < h->len; j++)
-		{
-			_putchar(str[j]);
-		}
-
-		_putchar('\n');
-
-		i++;
 		h = h->next;
+		i++;
 	}
 
 	return (i);
