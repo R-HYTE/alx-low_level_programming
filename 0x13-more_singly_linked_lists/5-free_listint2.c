cd4cb8547f0772a0553036fb1b80f61e45185255
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-29 20:44:31 +0300

    Had to fix the memory leak

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
 
-	if (head != NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		printf("(nil)\n");
 		return;
