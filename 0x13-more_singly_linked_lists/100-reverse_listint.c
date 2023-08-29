1d7cd46d214159d7f16990e6a38967451c5fcdbd
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-30 01:01:33 +0300

    Used pointer to a pointer to a node instead

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,15 +8,14 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
-	while (current)
+	while (*head)
 	{
-		next = current->next; /*Stores the next node*/
-		current->next = prev; /*Reverses the link*/
-		prev = current; /*move prev to the current node*/
-		current = next; /*Move current to the next node*/
+		next = (*head)->next; /*Stores the next node*/
+		(*head)->next = prev; /*Reverses the link*/
+		prev = (*head); /*move prev to the current node*/
+		(*head) = next; /*Move current to the next node*/
 	}
 
 	*head = prev; /*Update the head pointer to the new first node*/
