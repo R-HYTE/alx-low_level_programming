d8df05bb4b4a554a8b22146fd8b304786927035a
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-29 16:18:36 +0300

    Function that frees the heap

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees memory of the entire list
+ * @head - pointer to the first node
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
