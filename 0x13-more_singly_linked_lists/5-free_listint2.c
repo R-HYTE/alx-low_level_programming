ff334dd2a478b91dab12c6d088f817e75f461a67
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-29 20:49:21 +0300

    Bug fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,29 @@
 #include "lists.h"
 
 /**
+ * print_listint_safe - Prints the elements of a listint_t list safely
+ * @head: pointer to the first node
+ * Return: Number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	if (count == 0)
+		printf("nil\n"); /*Print "nil" if the list is empty*/
+
+	return count;
+}
+
+/**
  * free_listint2 - Frees memory location for the list
  * @head: pointer to a pointer to the first node
  * Return: void
