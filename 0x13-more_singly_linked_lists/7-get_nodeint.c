23f210169acf16f986caf9dd5ee85b113c826173
R_HYTE <polycarpwathuta39@gmail.com>
2023-08-29 23:55:39 +0300

    Bug fixed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int count = 0;
 
-	if (head == NULL)
-	{
-		printf("Error\n");
-		return (NULL);
-	}
-
 	current = head;
 	while (current)
 	{
