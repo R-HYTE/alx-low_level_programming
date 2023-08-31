6428fc9fa7cb32a0c42c256d740563b34299e6ce
R_HYTE <polycarpwathuta39@gmail.com>
2023-09-01 02:16:24 +0300

    Oh dear betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: value being referred to
+ * @index: position
+ * Return: 1 (Success), -1 (Failure)
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	/*Check if the index is within the valid range*/
@@ -10,7 +16,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	/*Create a mask with a 1 at the desired index*/
 	mask = 1UL << index;
-	*n &= ~mask; /*Use bitwise AND with the complement to clear the bit at the index*/
+
+	/*Use bitwise AND with the complement to clear the bit at the index*/
+	*n &= ~mask;
 
 	return (1);
 }
