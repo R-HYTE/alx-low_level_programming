33745000b8648b7b0c0dac2796956f7b734e4042
R_HYTE <polycarpwathuta39@gmail.com>
2023-09-05 19:22:20 +0300

    Changed the exit strategy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		print_error("Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		{
 			print_error("Error: Can't read from file %s\n", argv[1]);
 		}
-		return (98);
+		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 			print_error("Error: Can't write to %s\n", argv[2]);
 		}
 		close(fd_from);
-		return (99);
+		exit(99);
 	}
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 			print_error("Write error\n");
 			close(fd_from);
 			close(fd_to);
-			return (99);
+			exit(99);
 		}
 	}
 	if (bytes_read == -1)
@@ -62,17 +62,17 @@ int main(int argc, char *argv[])
 		print_error("Read error\n");
 		close(fd_from);
 		close(fd_to);
-		return (99);
+		exit(99);
 	}
 	if (close(fd_from) == -1)
 	{
 		print_error("Error: Can't close fd %u\n", fd_from);
-		return (100);
+		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
 		print_error("Error: Can't close fd %u\n", fd_to);
-		return (100);
+		exit(100);
 	}
 	return (0);
 }
