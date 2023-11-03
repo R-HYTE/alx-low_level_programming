24c14fee2dadd4db53c12d245bf481d63cae6e31
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-03 15:01:12 +0300

    function that adds an element to the hash table
    Oh dear betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -78,7 +78,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  */
 void free_node(hash_node_t *node)
 {
-        free(node->key);
-        free(node->value);
-        free(node);
+	free(node->key);
+	free(node->value);
+	free(node);
 }
