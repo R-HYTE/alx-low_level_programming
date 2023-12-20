8a70ea4e20b47220a7ae061c24fb299b8d52444b
R_HYTE <polycarpwathuta39@gmail.com>
2023-12-20 23:59:31 +0300

    Island Perimeter Technical Interview question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+    """
+
+    perimeter = 0
+
+    if not grid or not grid[0]:
+        return perimeter
+
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
